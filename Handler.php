6de21483b4195180b2cf1b50c736ca90731fe995
Youssef <youssefhad2@gmail.com>
2016-03-06 12:08:23 +0000

    try to find the way to clear all other errors

--- Handler.php
+++ Handler.php
@@ -10,7 +10,16 @@ class Handler
 	private $callback;
 	private static $ispassed=false;
 
-	function __construct()
+	// function __construct()
+	// {
+	// 	// $callback = call_user_func("Fiesta\Vendor\Error\Handler::run");
+	// 	set_error_handler(array($this,"run"));
+	// 	set_exception_handler(array($this, "excep"));
+	// 	register_shutdown_function(array($this, "reg"));
+		
+	// }
+
+	public function load()
 	{
 		// $callback = call_user_func("Fiesta\Vendor\Error\Handler::run");
 		set_error_handler(array($this,"run"));
@@ -19,25 +28,28 @@ class Handler
 		
 	}
 
-	public static function run()
+	public function run()
 	{
-		self::$ispassed=false;
+		self::$ispassed = false;
 		// ob_clean();
 		echo "<br>1";
 		$error=error_get_last();
+		// die(var_dump($error));
+		var_dump(debug_backtrace());
 		$exception =new \ErrorException ($error["message"]." /1 ",$error["code"],$error["code"], $error["file"], $error["line"]);
 		self::excep($exception);
 		echo "ERROR";
 		// die("run");
 	}
 
-	public static function reg()
+	public function reg()
 	{
 		if( ! self::$ispassed)
 		{
 			// ob_clean();
 			echo "<br>2";
 			$error=error_get_last();
+			var_dump(debug_backtrace());
 			if( ! is_null($error)) 
 			{
 				$exception =new \ErrorException ($error["code"]."/2",$error["code"],$error["code"], $error["file"], $error["line"]);
@@ -51,12 +63,14 @@ class Handler
 		// ;
 	}
 
-	public static function excep($exp)
+	public function excep($exp)
 	{
-		// ob_clean();
+		ob_clean();
 		self::$ispassed=true;
 		echo "<br>3";
-		var_dump($exp);
+		// var_dump($exp);
+		echo "<pre>";
+		print_r($exp->getTrace());
 		die("excep");
 	}
 }
